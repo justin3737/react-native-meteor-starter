b5faff0476088593df25d54070d6f854bb20b4ac
Justin <juns3700@gmail.com>
2017-10-27 18:40:35 +0800

    added RN files

---
+++ react-native/app/screens/FindNearMe.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Container from '../components/Container';
+import { Header } from '../components/Text';
+import LocateMeButton from '../components/LocateMeButton';
+import { connectAlert } from '../components/Alert';
+
+class FindNearMe extends Component {
+  handleGeolocationSuccess = (position) => {
+    console.log('latitude', position.coords.latitude);
+    console.log('longitude', position.coords.longitude);
+  };
+
+  handleGeolocationError = (error) => {
+    this.props.alertWithType('error', 'Error', error.message);
+  };
+
+  goToNearMe = () => {
+    navigator.geolocation.getCurrentPosition(
+      this.handleGeolocationSuccess,
+      this.handleGeolocationError,
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  };
+  render() {
+    return (
+      <Container>
+        <LocateMeButton
+          onPress={this.goToNearMe}
+        />
+        <Header>
+          Find Nearest Charging Stations
+        </Header>
+      </Container>
+    );
+  }
+}
+
+export default connectAlert(FindNearMe);
--- react-native/index.android.js
+++ react-native/index.android.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './app/index';
 
-AppRegistry.registerComponent('JSReactNative', () => App);
+AppRegistry.registerComponent('ESReactNative', () => App);
